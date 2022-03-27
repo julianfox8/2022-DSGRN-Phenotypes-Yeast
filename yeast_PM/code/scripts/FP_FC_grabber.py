309a15683bbca979617ab91f9ce1b83bbe9a0eb1
julianfox8 <julianfox8@gmail.com>
2022-03-27 16:57:33 -0600

    fc_fp

--- yeast_PM/code/scripts/FP_FC_grabber.py
+++ yeast_PM/code/scripts/FP_FC_grabber.py
@@ -1,47 +1,59 @@
 import DSGRN
 import sqlite3
-import json, sys, os
-
-from numpy import blackman
+import json, sys, os, tqdm
+from dsgrn_net_query.utilities.file_utilities import read_networks
 
 def query_filter(unfiltered_l):
     filtered_l = {}
-    for k,v in unfiltered_l: 
+    for k,v in unfiltered_l.items(): 
         if v.startswith("FP { 2") or v.startswith("FP { 3"): 
             pass 
         elif v.endswith("3, 1 }"): 
             pass 
-        elif v not in unfiltered_l: 
-            filtered_l[v] = [k] 
+        elif v not in filtered_l: 
+            filtered_l[k] = [v] 
         else: 
-            filtered_l[v].append(k)
+            filtered_l[k].append(v)
     return filtered_l
 
+def fp_queries(db):
+    if os.path.exists("bistable_fp_query.json") and os.path.exists("multistable_fp_query.json"):
+        multistable_fp =  json.load(open("multistable_fp_query.json"))
+        bistable_fp = json.load(open("bistable_fp_query.json"))
+    else:
+        c = sqlite3.connect(db)
+        cursor = c.cursor()
+        bistable_query = list(set([row[0] for row in cursor.execute("select ParameterIndex from Signatures natural join (select MorseGraphIndex, count(*) as StableCount from (select MorseGraphIndex,Vertex from MorseGraphVertices except select MorseGraphIndex,Source from MorseGraphEdges) group by MorseGraphIndex) where StableCount=2;")]))
+        multistable_query = list(set([row[0] for row in cursor.execute("select ParameterIndex from Signatures natural join (select MorseGraphIndex, count(*) as StableCount from (select MorseGraphIndex,Vertex from MorseGraphVertices except select MorseGraphIndex,Source from MorseGraphEdges) group by MorseGraphIndex) where StableCount>2;")]))
+        FP_query = dict(set([ row for row in cursor.execute('select ParameterIndex,label from Signatures natural join ( select MorseGraphIndex,label from MorseGraphAnnotations where label like "FP { _, 0, 2, _, 1 }" except select MorseGraphIndex,Source from MorseGraphEdges);')]))
+        query_filtered = query_filter(FP_query)
+        bistable_fp_keys = set(bistable_query).intersection(query_filtered.keys())
+        bistable_fp = {k:FP_query[k] for k in bistable_fp_keys}
+        with open("bistable_fp_query.json", "w") as f:
+            json.dump(bistable_fp, f)
+        multistable_fp_keys = set(multistable_query).intersection(query_filtered.keys())
+        multistable_fp = {k:FP_query[k] for k in tqdm.tqdm(multistable_fp_keys)}
+        with open("multistable_fp_query.json", "w") as f:
+            json.dump(multistable_fp, f)
+    return multistable_fp, bistable_fp
+
 def FC_FP_grabber(db,wt_pm, network, resultdir):
-    c = sqlite3.connect(db)
-    cursor = c.cursor()
-    bistable_query = list(set([row[0] for row in cursor.execute("select ParameterIndex from Signatures natural join (select MorseGraphIndex, count(*) as StableCount from (select MorseGraphIndex,Vertex from MorseGraphVertices except select MorseGraphIndex,Source from MorseGraphEdges) group by MorseGraphIndex) where StableCount=2;")]))
-    multistable_query = list(set([row[0] for row in cursor.execute("select ParameterIndex from Signatures natural join (select MorseGraphIndex, count(*) as StableCount from (select MorseGraphIndex,Vertex from MorseGraphVertices except select MorseGraphIndex,Source from MorseGraphEdges) group by MorseGraphIndex) where StableCount>2;")]))
-    FP_query = dict(set([ row for row in cursor.execute('select ParameterIndex,label from Signatures natural join ( select MorseGraphIndex,label from MorseGraphAnnotations where label like "FP { _, 0, 2, _, 1 }" except select MorseGraphIndex,Source from MorseGraphEdges);')]))
-    bistable_fp_keys = set(bistable_query).intersection(FP_query.keys)
-    bistable_fp = {k:FP_query for k in bistable_fp_keys}
-    multistable_fp_keys = set(multistable_query).intersection(FP_query.keys)
-    multistable_fp = {k:FP_query for k in multistable_fp_keys}
-    multistable_fp_filtered = query_filter(multistable_fp)
-    bistable_fp_filtered = query_filter(bistable_fp)
-    wt_plist = json.load(open(wt_pm))
+    multistable_fp,bistable_fp = fp_queries(db)
+    wt_dict = json.load(open(wt_pm))
+    net_spec = read_networks(network)
+    wt_plist = wt_dict[net_spec[0]]
     fc_fp_dict = {}
     for n,l in wt_plist:
-        bistable_list = {'bistable': {} }
-        multistable_list = {'multistable': {} }
-        for fp, bl in bistable_fp_filtered:
+        bistable_list = {'bistable': {}}
+        multistable_list = {'multistable': {}}
+        for fp, bl in tqdm.tqdm(bistable_fp.items()):
             bl_fp = list(set(l).intersection(bl))
-            bistable_list['bistable'][fp] = bl_fp
-        for fp, ml in multistable_fp_filtered:
+            bistable_list['bistable'][fp] = len(bl_fp)
+        for fp, ml in tqdm.tqdm(multistable_fp.items()):
             ml_fp = list(set(l).intersection(ml))
-            multistable_list['multistable'][fp] = ml_fp
+            multistable_list['multistable'][fp] = len(ml_fp)
         fc_fp_dict[n] = bistable_list
-        fc_fp_dict[n] = multistable_list
+        fc_fp_dict[n].update(multistable_list)
     net_name = os.path.splitext(os.path.basename(network))[0]
     fname = "{}_pheno_III.json".format(net_name)
     dir = os.path.join(resultdir, fname)
