df55c3b24005600b668b388027c58c1251923bb2
julianfox8 <julianfox8@gmail.com>
2022-04-27 16:10:14 -0600

    overall_remainder_final

--- yeast_PM/code/scripts/overall_remainder_check.py
+++ yeast_PM/code/scripts/overall_remainder_check.py
@@ -33,21 +33,21 @@ def mutant_hex_order(net,hi_pm,lo_pm,int_hi_pm,int_lo_pm):
     net_spec = read_networks(net)
     hi_plist = json.load(open(hi_pm))
     hi_plist = hi_plist[net_spec[0]]
-    hi_hex_order = hex_order_grabber(net,hi_plist[-1])
+    hi_hex_order = hex_order_grabber(net,hi_plist[-1][1])
     del hi_plist
     lo_plist = json.load(open(lo_pm))
     lo_plist = lo_plist[net_spec[0]]
-    lo_hex_order = hex_order_grabber(net,lo_plist[-1])
+    lo_hex_order = hex_order_grabber(net,lo_plist[-1][1])
     hi_lo_hex_order = hi_hex_order.intersection(lo_hex_order)
     del lo_plist,lo_hex_order, hi_hex_order
     int_hi_plist = json.load(open(int_hi_pm))
     int_hi_plist = int_hi_plist[net_spec[0]]
-    int_hi_hex_order = hex_order_grabber(net,int_hi_plist[-1])
+    int_hi_hex_order = hex_order_grabber(net,int_hi_plist[-1][1])
     hi_lo_int_hi_hex_order = hi_lo_hex_order.intersection(int_hi_hex_order)
     del int_hi_plist, int_hi_hex_order
     int_lo_plist = json.load(open(int_lo_pm))
     int_lo_plist = int_lo_plist[net_spec[0]]
-    int_lo_hex_order = hex_order_grabber(net,int_lo_plist[-1])
+    int_lo_hex_order = hex_order_grabber(net,int_lo_plist[-1][1])
     overall_mutant_hex_order = hi_lo_int_hi_hex_order.intersection(int_lo_hex_order)
     return overall_mutant_hex_order
 
@@ -59,7 +59,7 @@ def overall_remainder_comp(db,net,wt_pm,hi_pm,lo_pm,int_hi_pm,int_lo_pm):
     mutant_fp_hex_order = fp_hex_order.intersection(mutant_hex_order_list)
     wt_dict = json.load(open(wt_pm))
     wt_plist = wt_dict[net_spec[0]]
-    wt_hex_order = hex_order_grabber(net,wt_plist[-1])
+    wt_hex_order = hex_order_grabber(net,wt_plist[-1][1])
     overall_hex_order = mutant_fp_hex_order.intersection(wt_hex_order)
     net_name = os.path.splitext(os.path.basename(net))[0]
     f_name = "{}_overall_remainder.json".format(net_name)
