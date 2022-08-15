bc99aa1a5a286117e15eaa2b87627d60c5921556
julianfox8 <julianfox8@gmail.com>
2022-08-15 11:40:50 -0600

    pheno2_remainder

---
+++ yeast_PM/code/scripts/remainder_co_existing.py
@@ -0,0 +1,44 @@
+import json
+import DSGRN
+import sys, os, ast, sqlite3
+from dsgrn_net_query.utilities.file_utilities import read_networks
+from pyrsistent import mutant
+
+def hex_order_grabber(net,plist):
+    net = DSGRN.Network(net)
+    pg = DSGRN.ParameterGraph(net)
+    hex_orders = set()
+    for i in plist:
+        param = pg.parameter(i)
+        hex_order = tuple()
+        for j in range(net.size()):
+            if j == 3:
+                pass
+            else:
+                hex_order = hex_order + ((param.logic()[j].hex(),str(ast.literal_eval(str(param.order()[j])))))
+        hex_orders.add(hex_order)
+    return(hex_orders)
+
+def overall_remainder_comp(net,wt_pm,mutant_pm):
+    net_spec = read_networks(net)
+    mutant_plist = json.load(open(mutant_pm))
+    mutant_plist = mutant_plist[net_spec[0]]
+    mutant_hex_order = hex_order_grabber(net,mutant_plist[-1][1])
+    wt_dict = json.load(open(wt_pm))
+    wt_plist = wt_dict[net_spec[0]]
+    wt_hex_order = hex_order_grabber(net,wt_plist[-1][1])
+    overall_hex_order_set = mutant_hex_order.intersection(wt_hex_order)
+    print("remainder parameters = {}".format(len(overall_hex_order_set)))
+    overall_hex_order = list(overall_hex_order_set)
+    net_name = os.path.splitext(os.path.basename(net))[0]
+    f_name = "{}_remainder.json".format(net_name)
+    with open(f_name,'w') as f:
+        json.dump(overall_hex_order,f)
+
+if __name__ == '__main__':
+    net = sys.argv[1]
+    wt_pm_dict = sys.argv[2]
+    mutant_pm_dict = sys.argv[3]
+    fg_label = sys.argv[4]
+    resultdir = sys.argv[5]
+    overall_remainder_comp(net, wt_pm_dict, mutant_pm_dict, resultdir)
