f70d84f2431617308a758143d4a53349ccf01eca
julianfox8 <julianfox8@gmail.com>
2022-08-29 14:08:40 -0600

    fp_redo

--- src/scripts/func_pheno_III_overall_test.py
+++ src/scripts/func_pheno_III_overall_test.py
@@ -1,7 +1,6 @@
 import json
 import sys, os
 from dsgrn_pheno_tools.hex_order_grabber import hex_order_grabber
-from dsgrn_net_query.utilities.file_utilities import read_networks
 
 def hex_order_comp(net,wt_pm,fp_file, noise, resultdir=''):
     """
@@ -16,17 +15,16 @@ def hex_order_comp(net,wt_pm,fp_file, noise, resultdir=''):
     :output: dictionary .json file containing the number of matches at each noise level for that specific phenotype and proxy
     """
     wt_dict = json.load(open(wt_pm))
-    net_spec = read_networks(net)
-    wt_plist = wt_dict[net_spec[0][noise]]
+    wt_plist = wt_dict[0][-1]
     fp_dict = json.load(open(fp_file))
     fp_list = sum(fp_dict.values(),[])
     fp_hex_order = hex_order_grabber(net,fp_list)
     shared_hex_order = {}
     noise_hex_order = hex_order_grabber(net,wt_plist)
-    shared_hex_order[noise] = len(fp_hex_order.intersection(noise_hex_order))
+    shared_hex_order[noise] = fp_hex_order.intersection(noise_hex_order)
     net_name = os.path.splitext(os.path.basename(net))[0]
     f = os.path.basename(fp_file).split('_')[0]
-    filename = "{}_{}_overall_remainder_test.json".format(net_name,f)
+    filename = "{}_{}_overall_remainder_test_params.json".format(net_name,f)
     dir = os.path.join(resultdir, filename)
     with open(dir, "w") as f:
         json.dump(shared_hex_order,f)
