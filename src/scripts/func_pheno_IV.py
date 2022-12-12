fd5b50cb7a32f7dcaaad7687a2811f219f196ec0
julianfox8 <julianfox8@gmail.com>
2022-12-12 13:34:41 -0700

    overall3

--- src/scripts/func_pheno_IV.py
+++ src/scripts/func_pheno_IV.py
@@ -40,21 +40,21 @@ def fp_hex_order_grabber(net,plist):
 def mutant_hex_order(net,hi_pm,lo_pm,int_hi_pm,int_lo_pm):
     net_spec = read_networks(net)
     hi_plist = json.load(open(hi_pm))
-    hi_hex_order = hex_order_grabber(net,hi_plist[net_spec][-1][1])
+    hi_hex_order = hex_order_grabber(net,hi_plist[net_spec[0]][-1][1])
     print(len(hi_hex_order))
     del hi_plist
     lo_plist = json.load(open(lo_pm))
-    lo_hex_order = hex_order_grabber(net,lo_plist[net_spec][-1][1])
+    lo_hex_order = hex_order_grabber(net,lo_plist[net_spec[0]][-1][1])
     hi_lo_hex_order = hi_hex_order.intersection(lo_hex_order)
     print(len(hi_lo_hex_order))
     del lo_plist,lo_hex_order, hi_hex_order
     int_hi_plist = json.load(open(int_hi_pm))
-    int_hi_hex_order = hex_order_grabber(net,int_hi_plist[net_spec][-1][1])
+    int_hi_hex_order = hex_order_grabber(net,int_hi_plist[net_spec[0]][-1][1])
     print(len(int_hi_hex_order))
     hi_lo_int_hi_hex_order = hi_lo_hex_order.intersection(int_hi_hex_order)
     del int_hi_plist, int_hi_hex_order
     int_lo_plist = json.load(open(int_lo_pm))
-    int_lo_hex_order = hex_order_grabber(net,int_lo_plist[net_spec][-1][1])
+    int_lo_hex_order = hex_order_grabber(net,int_lo_plist[net_spec[0]][-1][1])
     print(len(hi_lo_int_hi_hex_order))
     overall_mutant_hex_order = hi_lo_int_hi_hex_order.intersection(int_lo_hex_order)
     return overall_mutant_hex_order
