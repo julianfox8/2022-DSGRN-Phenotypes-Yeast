43298296ca34c2294b1b69630a9cfe52a4523fec
julianfox8 <julianfox8@gmail.com>
2023-01-15 15:29:51 -0700

    updated_test2

--- src/bash_scripts/updated_script_test.sh
+++ src/bash_scripts/updated_script_test.sh
@@ -5,7 +5,7 @@
 python ~/2022-DSGRN-Phenotypes-Yeast/src/scripts/func_pheno_II.py ~/2022-DSGRN-Phenotypes-Yeast/yeast_PM/functional_phenotype_I/wild_type/ace2_nrm1/dsgrn_net_query_results20210913155435/queries20210913155435/query_results_stablefc_orlando2008_WildType_r1_trunc_param_list.json ~/2022-DSGRN-Phenotypes-Yeast/yeast_PM/functional_phenotype_II/clb2_ON/ace2_nrm1/dsgrn_net_query_results20210913155437/queries20210913155437/query_results_stablefc_bristow2014_cdc20_r1_trunc_param_list.json ~/2022-DSGRN-Phenotypes-Yeast/networks/WT_with_clb2/WT_clb2_nonE_ace2_nrm1.txt '3F'  ~/2022-DSGRN-Phenotypes-Yeast/test_results/
 
 #func_pheno_III_compiler
-python ~/2022-DSGRN-Phenotypes-Yeast/src/scripts/func_pheno_III_compiler.py ~/yeast_databases/WT_clb2_nonE.db
+python ~/2022-DSGRN-Phenotypes-Yeast/src/scripts/func_pheno_III_fp_compiler.py ~/yeast_databases/WT_clb2_nonE.db
 
 #func_pheno_III test
 #python ~/2022-DSGRN-Phenotypes-Yeast/src/scripts/func_pheno_III.py ~/2022-DSGRN-Phenotypes-Yeast/networks/WT_with_clb2/WT_clb2_nonE_swi5_nrm1.txt ~/2022-DSGRN-Phenotypes-Yeast/yeast_PM/functional_phenotype_I/wild_type/swi5_nrm1/dsgrn_net_query_results20210913155435/queries20210913155435/query_results_stablefc_orlando2008_WildType_r1_trunc_param_list.json ~/2022-DSGRN-Phenotypes-Yeast/src/scripts/fp_query.json '0.1' ~/2022-DSGRN-Phenotypes-Yeast/test_results/
